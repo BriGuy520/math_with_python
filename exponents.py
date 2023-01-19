9339d459aa70f59c245fe535d4cc4c5df2e969b7
Brian Falasz <bfalasz@yahoo.com>
2023-01-18 22:01:21 -0600

    adding a soluton to for loop exponents practice problem

--- exponents.py
+++ exponents.py
@@ -1,9 +1,16 @@
+from IPython.display import display, Math
+
 x = 5 
 y = 5.1
 
 print("{:.2f}".format(x**(3/4) * 4**y))
 print("{:.8f}".format(3**3 / x**y))
 print(10**(x - 4))
+
+display(Math("{x}^(3/4) * 4^y"))
+display(Math("3^3 * x^y"))
+display(Math("10^({x} - 4)"))
+
 
 
 
---
+++ for_loop_exponents.py
@@ -0,0 +1,5 @@
+base = 2
+
+for x in range(10):
+  
+  print("%g to the power of %g is %g" % (base, x, base**x))
