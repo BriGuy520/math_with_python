b28c647e8408a73cdc7d50f39ad3a24038aab90f
Brian Falaz <brianfalasz@gmail.com>
2023-01-20 10:03:05 -0600

    correcitng my ipython format so it displays the formulas correctly in the exponents file

--- exponents.py
+++ exponents.py
@@ -3,15 +3,13 @@ from IPython.display import display, Math
 x = 5 
 y = 5.1
 
-print("{:.2f}".format(x**(3/4) * 4**y))
-print("{:.8f}".format(3**3 / x**y))
-print(10**(x - 4))
+answer_one = "{:.2f}".format(x**(3/4) * 4**y)
+answer_two = "{:.8f}".format(3**3 / x**y)
+answer_three = 10**(x - 4)
 
-display(Math("{x}^(3/4) * 4^y"))
-display(Math("3^3 * x^y"))
-display(Math("10^({x} - 4)"))
-
-# adding comment
+display(Math("x^{3/4} \\times 4^y = %g"), answer_one)
+display(Math("3^3 \\times x^y = %g"), answer_two)
+display(Math("10^{x - 4} = %g"), answer_three)
 
 
 
