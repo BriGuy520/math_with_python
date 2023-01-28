a2587ba51a22ea0775e32b079bdc313d2336353a
Brian Falaz <brianfalasz@gmail.com>
2023-01-28 16:39:12 -0600

    adding some order of operations related problems

---
+++ order_operations.py
@@ -0,0 +1,17 @@
+print((4 + 4) * 28)
+print((4 + 3) * 2)
+print(4 + 3 * 2)
+
+
+# Quiz 
+
+print( (4 * 5) / (7 + 3)) # 2
+print(9 / (3 + 6) - 1) # 0
+
+## Order of Operations ##
+# Please (Parantheses)
+# Excuse (Exponents)
+# My (Multiplication)
+# Dear (Division)
+# Aunt (Addition)
+# Sally (Subtraction)
